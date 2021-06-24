436dd6e824a3af4e1bce68c5c53a2e6f80225475
mbahmodin <email@farrel.xyz>
2021-06-24 23:14:32 +0700

    make the code more readable

--- nik.py
+++ nik.py
@@ -3,7 +3,7 @@ import random
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-here = json.load(open(dir_path + "/" + 'data.json'))
-x = random.choice(here).split('|')
-print("Jumlah NIK & KK :", len(here))
-print("NIK : " + x[0] + "\nKK  : " + x[1])
+json_data = json.load(open(dir_path + "/" + 'data.json'))
+output_numbers = random.choice(json_data).split('|')
+print("Numbers of NIK & KK :", len(json_data))
+print("NIK : " + output_numbers[0] + "\nKK  : " + output_numbers[1])
